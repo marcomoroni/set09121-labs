5d96437bf70ff756c3531579e250357c5a86f865
Marco Moroni <marcomoroni394@gmail.com>
2018-01-16 15:27:48 +0000

    Render a simple sprite

--- practical_2/main.cpp
+++ practical_2/main.cpp
@@ -1,15 +1,25 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 using namespace sf;
 using namespace std;
 
+Texture spritesheet;
+Sprite invader;
+
 void Load() {
+	if (!spritesheet.loadFromFile("res/img/invaders_sheet.png")) {
+		cerr << "Failed to load spritesheet." << endl;
+	}
+	invader.setTexture(spritesheet);
+	invader.setTextureRect(IntRect(0, 0, 32, 32));
 }
 
 void Update(RenderWindow &window) {
 }
 
 void Render(RenderWindow &window) {
+	window.draw(invader);
 }
 
 int main() {
