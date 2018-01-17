1f0e3637103de68a7b37a2ef46a61e6810626468
Marco Moroni <marcomoroni394@gmail.com>
2018-01-17 14:09:38 +0000

    Implement one invader

--- practical_2/main.cpp
+++ practical_2/main.cpp
@@ -7,15 +7,14 @@ using namespace sf;
 using namespace std;
 
 Texture spritesheet;
-Sprite invader;
 std::vector<Ship *> ships;
 
 void Load() {
 	if (!spritesheet.loadFromFile("res/img/invaders_sheet.png")) {
 		cerr << "Failed to load spritesheet." << endl;
 	}
-	invader.setTexture(spritesheet);
-	invader.setTextureRect(IntRect(0, 0, 32, 32));
+	Invader* inv = new Invader(IntRect(0, 0, 32, 32), { 100,100 });
+	ships.push_back(inv);
 }
 
 void Update(RenderWindow &window) {
@@ -35,10 +34,17 @@ void Update(RenderWindow &window) {
 	if (Keyboard::isKeyPressed(Keyboard::Escape)) {
 		window.close();
 	}
+
+	// Ships
+	for (auto &s : ships) {
+		s->Update(dt);
+	}
 }
 
 void Render(RenderWindow &window) {
-	window.draw(invader);
+	for (auto &s : ships) {
+		window.draw(*s);
+	}
 }
 
 int main() {
