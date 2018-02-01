727647f0194bb9abc4658c56b6cf156e9bbbb318
Marco Moroni <marcomoroni394@gmail.com>
2018-02-01 14:10:31 +0000

    Working on tile lib...

---
+++ lib_tile_level_loader/levelsystem.cpp
@@ -0,0 +1,29 @@
+#include "levelsystem.h"
+#include <fstream>
+
+using namespace std;
+using namespace sf;
+
+std::unique_ptr<LevelSystem::TILE[]> LevelSystem::_tiles;
+size_t LevelSystem::_width;
+size_t LevelSystem::_height;
+
+float LevelSystem::_tileSize(100.f);
+vector<std::unique_ptr<sf::RectangleShape>> LevelSystem::_sprites;
+
+std::map<LevelSystem::TILE, sf::Color> LevelSystem::_colours{
+	{WALL, Color::White},
+	{END, Color::Red}
+};
+
+sf::Color LevelSystem::getColor(LevelSystem::TILE t) {
+	auto it = _colours.find(t);
+	if (it == _colours.end()) {
+		_colours[t] = Color::Transparent;
+	}
+	return _colours[t];
+}
+
+void LevelSystem::setColor(LevelSystem::TILE t, sf::Color c) {
+	// ...
+}
