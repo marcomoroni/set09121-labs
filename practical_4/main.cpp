f9e2845a306755fd54d64854202f22ce939ecf7f
Marco Moroni <marcomoroni394@gmail.com>
2018-02-07 15:30:11 +0000

    Empty pacman project

---
+++ practical_4/main.cpp
@@ -0,0 +1,47 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "game.h"
+
+using namespace sf;
+using namespace std;
+
+void Reset() {
+}
+
+void Load() {
+	Reset();
+}
+
+void Update(RenderWindow &window) {
+	// Reset clock, recalculate deltatime
+	static Clock clock;
+	float dt = clock.restart().asSeconds();
+	// Check and consume events
+	Event event;
+	while (window.pollEvent(event)) {
+		if (event.type == Event::Closed) {
+			window.close();
+			return;
+		}
+	}
+
+	// Quit via ESC Key
+	if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+		window.close();
+	}
+}
+
+void Render(RenderWindow &window) {
+}
+
+int main() {
+	RenderWindow window(VideoMode(gameWidth, gameHeight), "TILE ENGINE");
+	Load();
+	while (window.isOpen()) {
+		window.clear();
+		Update(window);
+		Render(window);
+		window.display();
+	}
+	return 0;
+}
