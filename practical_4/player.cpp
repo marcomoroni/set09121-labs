a74cbf03dd4f96ca27a51b7ef52590add77a1ecb
Marco Moroni <marcomoroni394@gmail.com>
2018-02-07 15:53:37 +0000

    Constant non-orthogonal movement speed

--- practical_4/player.cpp
+++ practical_4/player.cpp
@@ -18,6 +18,12 @@ void Player::update(float dt) {
 	if (Keyboard::isKeyPressed(Keyboard::Down)) {
 		displacement.y ++;
 	}
+	// Normalise displacement
+	float l = sqrt(displacement.x * displacement.x + displacement.y * displacement.y);
+	if (l != 0) {
+		displacement.x = displacement.x / l;
+		displacement.y = displacement.y / l;
+	}
 	move((float)dt * displacement * _speed);
 
 	Entity::update(dt);
