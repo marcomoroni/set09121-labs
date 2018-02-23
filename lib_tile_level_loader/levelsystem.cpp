019d9468eb394c886b1f06e4043ad0f3e8b1b396
Marco Moroni <marcomoroni394@gmail.com>
2018-02-23 16:56:10 +0000

    Working on LevelSystem::findTiles...

--- lib_tile_level_loader/levelsystem.cpp
+++ lib_tile_level_loader/levelsystem.cpp
@@ -141,10 +141,13 @@ LevelSystem::TILE LevelSystem::getTileAt(Vector2f v) {
 
 std::vector<sf::Vector2ul> LevelSystem::findTiles(LevelSystem::TILE tile) {
 	std::vector<sf::Vector2ul> ret;
-	for (auto t : *_tiles) {
-		if (t == tile) {
-			ret.push_back(t);
+	for (int i = 0; i < sizeof(&_tiles); i++) {
+		if (_tiles[i] == tile) {
+			ret.push_back(Vector2ul(i % _width, (_width / (i + 1)) - 1));
 		}
+	}
+	if (ret.size() == 0) {
+		throw string("No tiles found.");
 	}
 	return ret;
 }
